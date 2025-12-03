2a08e249d54f4ed049b33f8902425d06bdc22e90
Fiona Cutler <fiona.r.cutler@outlook.com>
2025-12-03 14:53:15 -0700

    Added theme

---
+++ constants/theme.ts
@@ -0,0 +1,15 @@
+
+
+const tintColorLight = '#0a7ea4';
+const tintColorDark = '#fff';
+
+export const Colors = {
+  light: {
+    text: '#11181C',
+    background: '#fff',
+  },
+  dark: {
+    text: '#ECEDEE',
+    background: '#151718'
+  },
+};
