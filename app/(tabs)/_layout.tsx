6c7e9eb3217c8ec5e4d6977b838dfea7121469a8
Fiona Cutler <fiona.r.cutler@outlook.com>
2025-12-04 16:40:34 -0700

    Basic tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,24 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+  <Tabs       
+    screenOptions={{
+        tabBarActiveTintColor: '#cc5500',
+      }} >
+    <Tabs.Screen name="index" 
+    options={{ 
+        title:'Home', 
+        tabBarIcon: ({color,focused}) =>(
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+        ),
+     }}/>    <Tabs.Screen name="info" 
+    options={{ 
+        title:'Info', 
+        tabBarIcon: ({color,focused}) =>(
+            <Ionicons name={focused ? 'information-circle': 'information-circle-outline'} color={color} size={24} />
+        ),
+     }}/>
+  </Tabs>);
+}
